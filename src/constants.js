cb41929cddaba6165b5d2316aa680e8840162d96
dharshinis2002 <113338483+dharshinis2002@users.noreply.github.com>
2024-07-05 15:46:59 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,49 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Editor",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Dharshini';
+
+export const experiences = [
+    {
+        'company': 'Mahathi Infotech',
+        'role': 'Junior Software Engineer',
+        'duration': 'May 2023 - Present',
+        'logo': 'https://www.mahathiinfotech.com/assets/images/mahathi_logo2.png',
+        'points': [
+            'Involved in dynamic contributions to web 📱 development projects, actively influencing and improving project outcomes.',
+            'Showed innovation and problem-solving abilities by optimizing the submission creation process, streamlining workflows and improving efficiency.',
+            'Enhanced development performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
+            'Participating in continuing education and training to remain current on best practices, learn new programming languages, and better assist other team members .⚙️.'
+        ],
+        'url': 'https://inncircles.com/#home',
+    },
+    {
+        'company': 'Ebrain Technologies',
+        'role': 'Java Beackend Developer Intern',
+        'duration': 'Sep 2022 - April 2023',
+        'logo': 'https://ebraintechnologies.com/assets/img/Ebrain/logoHeader.png',
+        'points': [
+            'As a Java Backend developer, I was responsible for developing and maintaining Java-based applications using Spring Boot framework.',' I worked extensively with MySQL databases to ensure efficient data storage and retrieval. Additionally, I played a key role in designing and implementing REST APIs to facilitate communication between different components of the system.📱.'
+        ],
+        'url': 'https://www.att.com/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_90wb9t5';
+export const EMAIL_JS_TEMPLATE_ID = 'template_8l87rew';
+export const EMAIL_JS_PUBLIC_KEY = 'pHPjc9RGGkF66r86N';
